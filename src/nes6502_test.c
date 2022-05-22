758bd9cdecd1ebfbdb02bcfb4921258541d24681
Josh Leath <joshua.leath@icloud.com>
2022-05-22 18:05:44 -0400

    main driver for generating test output

--- src/nes6502_test.c
+++ src/nes6502_test.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
 
 #include "nes6502.h"
+#include "code_reader.h"
 
 void print_test_result(char *test_name, uint16_t expected, uint16_t got) {
     if (expected != got) {
@@ -8,100 +11,96 @@ void print_test_result(char *test_name, uint16_t expected, uint16_t got) {
     }
 }
 
+uint8_t convert_hex(uint8_t byte)  {
+    if (byte >= '0' && byte <= '9') {
+        return byte - '0';
+    } else if (byte >= 'A' && byte <= 'F') {
+        return byte - 'A' + 10;
+    } else if (byte >= 'a' && byte <= 'f') {
+        return byte - 'a' + 10;
+    } else {
+        return -1;
+    }
+}
+
+uint8_t translate_code(char *input, uint8_t *output) {
+    uint8_t curr_val;
+    int curr_hex = 0;
+    for (int i = 0; input[i] != '\0'; i++) {
+        if (input[i] == ' ') {
+            output[curr_hex] = curr_val;
+            curr_val = 0;
+            curr_hex++;
+        } else {
+            curr_val = (curr_val << 4) | (convert_hex(input[i]) & 0x0f);
+        }
+    }
+    output[curr_hex] = curr_val;
+    return curr_hex + 1;
+}
+
 int main(void) {
     Bus *bus = create_bus(64*1024);
     Nes6502 cpu;
     cpu.bus = bus;
-    cpu.cycles = 0;
-    cpu.pc = 0;
-    cpu.a = 0;
-    cpu.x = 0;
-    cpu.y = 0;
 
-    // Testing addressing modes
-    // Accumulator
-    cpu.a = 42;
-    print_test_result("Accumulator", 42, accumulator(&cpu));
+    cpu.bus->ram[0xfffc] = 0x00;
+    cpu.bus->ram[0xfffd] = 0xc0;
 
-    // Immediate
-    cpu.pc = 0;
-    print_test_result("Immediate", 0, immediate(&cpu));
+    reset(&cpu);
 
-    // Zero Page
-    write(bus, 0x12, 0x42);
-    cpu.pc = 0x12;
-    print_test_result("Zero Page", 0x42, zero_page(&cpu));
+    // char *filename = "nestest.nes";
+    // off_t file_size = get_file_size(filename);
+    // if (file_size == -1) {
+    //     printf("Unable to get file size of %s\n", filename);
+    //     return -1;
+    // }
+    // uint8_t *code_buffer = read_program_file(filename, file_size);
+    // if (code_buffer == NULL) {
+    //     printf("failed to open and read file %s\n\n", filename);
+    //     return -2;
+    // }
 
-    // Zero Page X
-    cpu.x = 0x0f;
-    cpu.pc = 0;
-    write(bus, 0x00, 0x80);
-    print_test_result("Zero Page X", 0x8f, zero_pageX(&cpu));
-    cpu.x = 0xff;
-    cpu.pc = 0;
-    print_test_result("Zero Page X", 0x7f, zero_pageX(&cpu));
-
-    // Zero Page Y
-    cpu.y = 0x0f;
-    cpu.pc = 0;
-    write(bus, 0x00, 0x80);
-    print_test_result("Zero Page Y", 0x8f, zero_pageY(&cpu));
-    cpu.y = 0xff;
-    cpu.pc = 0;
-    print_test_result("Zero Page Y", 0x7f, zero_pageY(&cpu));
-
-    // Absolute
-    cpu.pc = 0;
-    write(bus, 0, 0x34);
-    write(bus, 1, 0x12);
-    print_test_result("Absolute", 0x1234, absolute(&cpu));
-
-    // Absolute X
-    cpu.pc = 0;
-    cpu.x = 0x92;
-    write(bus, 0, 0x00);
-    write(bus, 1, 0x20);
-    print_test_result("Absolute X", 0x2092, absoluteX(&cpu));
-
-    // Absolute Y
-    cpu.pc = 0;
-    cpu.y = 0x92;
-    write(bus, 0, 0x00);
-    write(bus, 1, 0x20);
-    print_test_result("Absolute Y", 0x2092, absoluteY(&cpu));
-
-    // Indirect
-    cpu.pc = 0;
-    write(bus, 0, 0x34);
-    write(bus, 1, 0x12);
-    write(bus, 0x1234, 0x78);
-    write(bus, 0x1235, 0x56);
-    print_test_result("Indirect", 0x5678, indirect(&cpu));
-    cpu.pc = 0;
-    write(bus, 0, 0xff);
-    write(bus, 1, 0x12);
-    write(bus, 0x12ff, 0x78);
-    write(bus, 0x1200, 0x56);
-    print_test_result("Indirect (bugged)", 0x5678, indirect(&cpu));
-
-    // Indirect X
-    cpu.pc = 0;
-    write(bus, 0, 0x51);
-    cpu.x = 0xe9;
-    write(bus, 0x3104, 0x81);
-    write(bus, 0x3a, 0x04);
-    write(bus, 0x3b, 0x31);
-    print_test_result("Indirect X", 0x3104, indirectX(&cpu));
-
-
-    // Indirect Y
-    cpu.pc = 0;
-    write(bus, 0, 0xa4);
-    write(bus, 0xa4, 0x51);
-    write(bus, 0xa5, 0x3f);
-    cpu.y = 0xe9;
-    write(bus, 0x3104, 0x81);
-    write(bus, 0x3a, 0x04);
-    write(bus, 0x3b, 0x31);
-    print_test_result("Indirect Y", 0x403a, indirectY(&cpu));
+    // // off_t pc = 0;
+    // // while (pc < file_size) {
+    // //     printf("%02x ", code_buffer[pc]);
+    // //     if (pc % 30 == 0 && pc != 0) {
+    // //         printf("\n");
+    // //     }
+    // //     pc++;
+    // // }
+    // // printf("\n");
+    // temp cycle adjustment just to match test output
+    cpu.total_cycles = 7;
+    // reset flags to match test
+    cpu.flags = 0x24;
+    uint8_t values[3];
+    while (!complete(&cpu)) {
+        cycle(&cpu);
+    }
+    int instructions_read = 0;
+    while (instructions_read < 8991) {
+        instructions_read++;
+        char input[9];
+        int c;
+        int curr_char = 0;
+        while ((c = getchar()) != '\n' && c != EOF) {
+            if (curr_char < 8) {
+                input[curr_char] = c;
+                curr_char++;
+            }
+        }
+        input[curr_char] = '\0'; 
+        int num_bytes = translate_code(input, values);
+        uint16_t p = cpu.pc;
+        for (int i = 0; i < num_bytes; i++) {
+            cpu.bus->ram[p] = values[i];
+            p++;
+        }
+        do {
+            cycle(&cpu);
+        } while (!complete(&cpu));
+    }
+    // free(code_buffer);
+    return 0;
 }
